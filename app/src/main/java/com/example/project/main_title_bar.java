7be7622d6df330517578849ada1f2fb5b8ab31be
fuhua <1472866286@qq.com>
2020-10-28 13:57:19 +0800

    初始化

---
+++ app/src/main/java/com/example/project/main_title_bar.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class main_title_bar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_title_bar);
+    }
+}
